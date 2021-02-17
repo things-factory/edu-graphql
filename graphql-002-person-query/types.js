569a8b7ee52e0372d22f3697fd8b66ea2c229dcf
heartyoh <heartyoh@hatiolab.com>
2021-02-17 16:34:52 +0900

    add description sample

--- graphql-002-person-query/types.js
+++ graphql-002-person-query/types.js
@@ -1,6 +1,10 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
+  """
+  Person에 대해서 설명할 수 있습니다.
+  여러줄 설명이 필요한 경우에는 이렇게 표현할 수 있습니다.
+  """
   type Person {
     id: String
     name: String
--- graphql-004-profile-field-query/types.js
+++ graphql-004-profile-field-query/types.js
@@ -2,6 +2,7 @@ const gql = require("graphql-tag");
 
 const typeDefs = gql`
   extend type Person {
+    "Person의 아바타 이미지 정보입니다."
     avatar: String
   }
 `;
